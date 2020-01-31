d716d2132fda7aea062cf2adc7aeef85699f9ccd
Adrian Schwizgebel <adrian.schwizgebel@gmail.com>
2020-01-31 16:20:18 +0100

    Update nonogramm.py

--- nonogramm.py
+++ nonogramm.py
@@ -5,24 +5,13 @@ class Nonogramm:
     def __init__(self, size, inputInfo):
         self.sizeX = size[0]
         self.sizeY = size[1]
-        self.field = [[0 for x in range(self.sizeX)] for y in range(self.sizeY)]
-        #self.inputInfoX = [[0 for x in range(self.sizeX)] for y in range(3)]
         self.inputInfoX = inputInfo[0]
-        #self.inputInfoX = [[1,1],[1],[4],[1],[2,1],[2,1]]
-        #self.inputInfoX = [[1],[2],[1]]
-        #self.inputInfoX = [[1],[1]]
-        #self.inputInfoX = [[1],[2],[2],[1,2]]
-        #self.inputInfoX = [[1],[1],[1],[1]]
-        #self.inputInfoX = [[1,1],[1],[3,1],[2,2],[1,1]]
-        #self.inputInfoY = [[0 for x in range(self.sizeY)] for y in range(3)]
         self.inputInfoY = inputInfo[1]
-        #self.inputInfoY = [[1,1,2],[1,2],[3],[4]]
-        #self.inputInfoY = [[1],[2],[1]]
-        #self.inputInfoY = [[1],[1]]
-        #self.inputInfoY = [[1,1],[3],[2],[1]]
-        #self.inputInfoY = [[4],[0],[0],[0]]
-        #self.inputInfoY = [[1,2],[3],[1,1],[1,1],[3]]
-        #self.solved = 0
+        self.field = [[0 for x in range(self.sizeX)] for y in range(self.sizeY)]
+        self.fieldFound = self.field
+        # 0: unknown
+        # 1: full
+        # 2: empty
     
     def printNG(self):
         print("Testprint.")
@@ -36,6 +25,19 @@ class Nonogramm:
         print(self.inputInfoX)
         print("inputInfoY")
         print(self.inputInfoY)
+    
+    def findBeginning(self):
+        for y in range(self.sizeY):
+            if sum(self.inputInfoY[y]) + len(self.inputInfoY[y]) - 1 == self.sizeX:
+                tempInputInfoX = []
+                tempInputInfoX = self.inputInfoX[y]
+                for item in tempInputInfoX:
+                    if item >= 2:
+                        print("one")
+                        pos = tempInputInfoX.index(item)
+                        tempInputInfoX[pos:pos+1] = (1, item - 1)
+                self.fieldFound[y] = tempInputInfoX
+                self.field[y] = self.fieldFound[y]
     
     def solveRandom(self):
         # 0.535ms for 2x2 in average
@@ -55,27 +57,15 @@ class Nonogramm:
             for x in range(self.sizeX):
                 self.field[y][x] = int(round(random.randint(0,1) + ((sum(self.inputInfoY[y]) / self.sizeX) - 0.5)))
     
-    def check(self):
-        guard = 0
-        
-        horizontalSum = 0
+    def solveOnlyUnknownRandom(self):
         for y in range(self.sizeY):
-            if sum(self.field[y]) == sum(self.inputInfoY[y]):
-                horizontalSum += 1
-        if horizontalSum == self.sizeY:
-            guard += 1
-        
-        verticalSum = 0
-        for x in range(self.sizeX):
-            sumField = 0
-            for y in range(self.sizeY):
-                sumField = sumField + self.field[y][x]
-            if sumField == sum(self.inputInfoX[x]):
-                verticalSum += 1
-            
-        if verticalSum == self.sizeX:
-            guard += 1
-        
+            for x in range(self.sizeX):
+                if self.fieldFound[y][x] == 1 or 2:
+                    self.field[y][x] = self.fieldFound[y][x]
+                else:
+                    self.field[y][x] = random.randint(0,1)
+    
+    def check(self):
         horizontalSpacing = 0
         for y in range(self.sizeY):
             foundinputInfoY = []
@@ -93,7 +83,9 @@ class Nonogramm:
             if foundinputInfoY == self.inputInfoY[y]:
                 horizontalSpacing += 1
         if horizontalSpacing == self.sizeY:
-            guard += 1
+            horizontalSpacing = 1
+        else:
+            horizontalSpacing = 0
         
         verticalSpacing = 0
         for x in range(self.sizeX):
@@ -112,21 +104,24 @@ class Nonogramm:
             if foundinputInfoX == self.inputInfoX[x]:
                 verticalSpacing += 1
         if verticalSpacing == self.sizeX:
-            guard += 1
+            verticalSpacing = 1
+        else:
+            verticalSpacing = 0
         
-        if guard == 4:
+        if horizontalSpacing == 1 and verticalSpacing == 1:
+            self.fieldFound = self.field
             return 1
         else:
             return 0
 
 print("Starting session.")
 
-gameNumber = 10
+gameNumber = 1
 startTime = time.time()
 
 
-inputInfo = [[[1], [2], [2], [1, 2]],
-         [[1, 1],
+inputInfo = [[[2], [2], [3], [1, 2]],
+         [[4],
           [3],
           [2],
           [1]
@@ -143,7 +138,8 @@ for x in range(gameNumber):
     NG = Nonogramm(size, inputInfo)
     
     while(NG.check() == 0):
-        NG.solveRandomBetter()
+        NG.findBeginning()
+        NG.solveOnlyUnknownRandom()
     
     print("\nSolution:")
     NG.printField()
